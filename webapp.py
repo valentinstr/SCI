8f8f3b5e25d1a8501a6a7819544d1143ed07c63d
valentinstr <47829007+valentinstr@users.noreply.github.com>
2023-01-17 01:34:37 +0100

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -6,20 +6,19 @@ st.title("Récupérer des sci")
 nom_association = st.text_input("Nom de l'association", "Association diocèsaine de Rennes")
 fermée = st.checkbox("Fermée", value=False)
 
+def convert_df(df):
+   return df.to_csv(index=False)
+
 if st.button("Récupérer les informations"):
     sci.get_sci(nom_association, fermée)
     st.write(sci.df_sci)
+    csv = convert_df(sci.df_sci)
+    st.download_button(
+    "Enregistrer le fichier",
+    csv,
+    f"scis_{nom_association.replace(' ','_')}.csv",
+    "text/csv",
+    key='download-csv'
+    )
 
-def convert_df(df):
-   return df.to_csv(index=False).encode('utf-8')
 
-
-csv = convert_df(sci.df_sci)
-
-st.download_button(
-   "Enregistrer le fichier",
-   csv,
-   "scis.csv",
-   "text/csv",
-   key='download-csv'
-)
